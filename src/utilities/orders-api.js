6d472d99a947d5b96f1a000995d3671799631068
Abiboulayed <79129877+Hackerbibou@users.noreply.github.com>
2023-07-03 21:55:19 -0400

    m

--- src/utilities/orders-api.js
+++ src/utilities/orders-api.js
@@ -1,15 +1,15 @@
-import sendRequest from './send-request';
+// import sendRequest from './send-request';
 
-const BASE_URL = '/api/orders';
+// const BASE_URL = '/api/orders';
 
-export function getCart() {
-    return sendRequest(`${BASE_URL}/cart`);
-}
+// export function getCart() {
+//     return sendRequest(`${BASE_URL}/cart`);
+// }
 
-export function addItemToCart(itemId) {
-    return sendRequest(`${BASE_URL}/cart/items/${itemId}`, 'POST');
-}
+// export function addItemToCart(itemId) {
+//     return sendRequest(`${BASE_URL}/cart/items/${itemId}`, 'POST');
+// }
 
-export function checkout() {
-    return sendRequest(`${BASE_URL}/cart/checkout`, 'POST');
-}
+// export function checkout() {
+//     return sendRequest(`${BASE_URL}/cart/checkout`, 'POST');
+// }
