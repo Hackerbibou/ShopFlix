b79481c4a97620ea0c08dd6252cfb8226250de63
Abiboulayed <79129877+Hackerbibou@users.noreply.github.com>
2023-07-26 21:35:07 -0400

    logo fixed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(cors({
-  origin:['https://localhost3000','https://shopflix.onrender.com']
+  origin:['https://localhost:3000','https://shopflix.onrender.com']
   // ,'https://shopflix.onrender.com'
 
 }))
--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -23,7 +23,7 @@ useEffect(()=>{
             animate={{opacity:1}}
             exit={{opacity:0}}
     className='menuBar'>
-        <Link to='/'><img  className="navLogo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo"></img></Link>
+        <Link to='/'><img  className="navLogo" src="https://lh3.googleusercontent.com/pw/AIL4fc9XoAYrysFjZIE9l0kQdAJeBoJE2kIZKJyljRX5KEgLdxJ-7J0SCo6Ekp93hIbRPf31ET7ItyW7YjNk9zKFZK2HU7PxSX-NleGnUXpem4Wm7guQ_f0DaCdv-NPWpUeog0cnOn8DVl2H_FTWY939l173Pf2ldgtVoC8VPV7zhn4Yb3qkBjrHi2tUAYu4OOOxWJvCKe9obqT1ats0X4oukhYsOyE3bDvfAl084DUBGoHjzkY8Cr8hoq6eb8h-w6bDkaEqAYdPAQZKj-JNV9oUxcmfeKB1D4gQuS1W7HcmT10g9h289Ul3BlchXy4h4dh0RbRRdx6bFUTZEW3qMgaeS3BVVNdUTHnuftTo-CP0RtDN_NOPT8mPmuq1HR11cxYhi9K6QtP0E73uQDDqaEEH2Xi-CQcTeimkaI5BBKuItq1FYYvh9FZphkk_SIHmS9uBdOb7oCPhvtcLAX7mJGiJaXhLK66YoQ48br35PXPra6QUkfHxq1z1ZiZ4YPRK4MgV3i0VSChbL6Dswl8JC_PruOJ4UIWhZgVmkakRwuNKlMHi097l2az9dk5M4QxHUoo1GbRkZlkLwMradSjkFQOaxIUjeddiwSGJbCYKGvBzQeqmirK_IrtPn5FnkyIB-Anr2QRcoxaJlWVAFlDOAELDZW5MtzFuIEL32IHGPWWxgzHI9B84-YKiqmAvwRu_iHKbntAbJmy8NcD4mzSHmLeHZPd0H8-h3bDe0KXFSiXvVKx_YEXuqXOmBLoz_Hm70hSvvo_Yyj4L83x_LctMOaRNVk3K6MdEeI01WaFcoG_Y6QkYN_xqwBrj0PG8jw6KI0hXkbnilSn9Q0ly5o6czQWYPBDQtNL1WIN372VRiqj-toSSenomkcpuXTPvqGi9hG5b9g3mJYjfQdV9nfYwSHd9vhVoB0SIvhscfsjOaSeVSMtZe4mi5MvNg8WUGUzt0qcEH47-GFMQ2oeV4TXm03Y3q5xosHQ6aK8=w1515-h549-s-no?authuser=0" alt="Netflix Logo"></img></Link>
         <div className='menuBarFlex'>
         
         <Link to='/search'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="28x28" fill="#d1d1d1" class="mercado-match" width="28" height="28" focusable="false">
